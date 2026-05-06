b4fdb5325c9d68008a7d3ae10518a3558ef8b206
dainireR <101051579+dainireR@users.noreply.github.com>
2026-05-06 10:49:37 +0300

    Update DARBS_V3.py

--- DARBS_V3.py
+++ DARBS_V3.py
@@ -10,10 +10,10 @@ with open("uzd3.json.","r",encoding="utf-8") as datne:
 uznemumi=[]
 vid_cena=[]
 for i in dati:
-    uznemumi.append("SIA laiks")
-    vid_cena=sum(cenas)/len(cenas)
+    uznemumi.append("SIA laiks")# jābūt atslēgas nosaukumam, lai pievienotu vērtību, i["atslēga"]
+    vid_cena=sum(cenas)/len(cenas) #katram uzņēmuma vid jāpievieno sarakstam, vērtības tiek pārrakstītas pa virsu viena otrai
 
-with open("akciju_analize.csv","w",encoding="utf-8"):
+with open("akciju_analize.csv","w",encoding="utf-8"):   # nepievienoji as datne:ir atvērts, bet netiek ierakstīts.
 
 
 
